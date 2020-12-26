35240486ca75f2fea8f9db229e8790e60cef4479
overlord-17 <66298422+overlord-17@users.noreply.github.com>
2020-12-26 20:49:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".leftColor");
+var color2 = document.querySelector(".rightColor");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
